0ce9cece3ff43078e3fbf8c868ed0474acd37ece
scorpion3013 <coc909scorp@gmail.com>
2020-11-13 15:31:31 +0100

    Test

--- react_close/react_close.py
+++ react_close/react_close.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-
+import discord
 
 class ReactionClose(commands.Cog):
     def __init__(self, bot):
@@ -10,7 +10,21 @@ class ReactionClose(commands.Cog):
         msg = thread.genesis_message
         print(msg)
         print(thread)
+        await msg.add_reaction("🔒")
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if not payload.guild_id:
+            return
+        emote = payload.emoji.name if payload.emoji.id is None else str(payload.emoji.id)
+        emoji = payload.emoji.name if payload.emoji.id is None else payload.emoji
 
+        guild = self.bot.get_guild(payload.guild_id)
+        member = discord.utils.get(guild.members, id=payload.user_id)
+
+        if member.bot:
+            return
+        print(emote)
+        print(emoji)
 def setup(bot):
     bot.add_cog(ReactionClose(bot))
